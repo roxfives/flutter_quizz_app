412787a07c979049f1be4ce51be52908fdcf742e
Ricardo Chagas <ricardo.chagas2@outlook.com>
2020-04-07 16:33:45 -0300

    My first widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(home: Scaffold(
+        appBar: AppBar(
+            title: Text('My First App'),
+          ),
+        body: Text('This i my body text'),
+      )
+    );
   }
 }
